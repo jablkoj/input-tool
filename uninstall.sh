d49c1b53aeaba72d0cb2b9f03481f1582833a759
jablkoj <jablko.jahoda@gmail.com>
2015-10-06 15:39:50 +0200

    Instalation

---
+++ uninstall.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+function color {
+    if [ -n "$1" ]; then
+        echo -e -n "\033[$1;1m"
+    else
+        echo -e -n "\033[m"
+    fi
+}
+green=`color 32`
+blue=`color 34`
+red=`color 31`
+normal=`color`
+
+function error {
+    echo ${red}${1}${normal}
+    exit 1
+}
+
+path="/usr/local/bin"
+files="input-tester" #input-sample" # input-generator"
+for f in $files; do
+    cmd="sudo rm $path/$f"
+    echo $cmd
+    $cmd || error "Failed"
+done
+
+echo "${green}Done${normal}"
