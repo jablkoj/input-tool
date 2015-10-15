6c8add70523fcbb9207d4d942c2f38b34d78dfbc
jablkoj <jablko.jahoda@gmail.com>
2015-10-15 17:43:43 +0200

    generator dostava vstup aj s koncom riadku

--- common/commands.py
+++ common/commands.py
@@ -342,7 +342,7 @@ class Generator(Program):  # {{{
     def generate(self, ifile, text):
         cmd = "%s > %s" % (self.runcmd, ifile)
         p = subprocess.Popen(cmd, stdin=subprocess.PIPE, shell=True)
-        p.communicate(str.encode(text))
+        p.communicate(str.encode(text+'\n'))
 
 #}}}
 
