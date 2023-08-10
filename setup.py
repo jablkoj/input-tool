7571b4c207b20db148f88b63e7ae48d80a3d650e
jablkoj <jablko.jahoda@gmail.com>
2023-08-10 23:33:01 +0200

    Update version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open("README.md", "r") as f:
 
 setup(
     name='input-tool',
-    version='1.0.0',
+    version='1.1.0',
     description=DESCRIPTION,
     long_description=long_description,
     long_description_content_type="text/markdown",
